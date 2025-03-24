0c620e71f0eea4ca7570ed4c582fb11d0d693dfe
tost74 <55083159+tost74@users.noreply.github.com>
2025-03-24 20:46:50 +0100

    Add files via upload

---
+++ DFS.py
@@ -0,0 +1,17 @@
+# Simple example of DFS with an unregular tree
+g = [[1, 2, 3], [4], [5, 6, 7], [8], [9, 10], [], [11, 12], [13], [14], [], [15, 16], [], [17], [], [18], [], [], [], []]
+
+
+def DFS(graph, visited=[], stack=[0]):
+    if stack == []:
+        print(visited)
+    else:
+        top = stack[-1]
+        stack.pop(-1)
+        visited.append(top)
+        turn = reversed(graph[top])
+        for i in turn:
+            stack.append(i) 
+        return DFS(graph, visited, stack)
+
+DFS(g, [], [0])
