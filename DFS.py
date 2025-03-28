80a79df6eec9377998c6868f933e2c51b42d9d6a
tost74 <55083159+tost74@users.noreply.github.com>
2025-03-28 18:44:07 +0100

    DFS.py

--- DFS.py
+++ DFS.py
@@ -1,4 +1,4 @@
-# Simple example of DFS with an unregular tree
+# Simple example of DFS with an irregular tree
 g = [[1, 2, 3], [4], [5, 6, 7], [8], [9, 10], [], [11, 12], [13], [14], [], [15, 16], [], [17], [], [18], [], [], [], []]
 
 
